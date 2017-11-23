5ae8a6cc533c93825f90d9981707d5d4b27c7870
englsa <arsenal4990@gmail.com>
2017-11-23 14:21:05 +0700

    lesson:10

---
+++ src/app/admin/homeadmin/homeadmin.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeadminComponent } from './homeadmin.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HomeadminComponent]
+})
+export class HomeadminModule { }
---
+++ src/app/admin/useradmin/useradmin.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UseradminComponent } from './useradmin.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UseradminComponent]
+})
+export class UseradminModule { }
